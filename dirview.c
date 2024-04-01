88621a84545d5a8569760e11af8c604711bce871
LollosaurusRex <ethanrex1@gmail.com>
2024-04-01 00:29:33 -0500

    Experimented more with pen colors

--- dirview.c
+++ dirview.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 
 #include <tickit.h>
+
+#define streq(a,b) (!strcmp(a,b))
+
+TickitPenRGB8 blue = {.r = 10, .g = 100, .b = 200};
 
 /**
  * I've never seen a struct created like this before.
@@ -28,8 +33,8 @@ struct
 	TickitPen *pen_fg, *pen_fg_hi, *pen_bg, *pen_bg_hi;
 } penColors[] = 
 {
-	{"blue", 212},
-	{"red", 4}
+	{"blue", 1},
+	{"red", 1}
 };
 
 
@@ -47,21 +52,46 @@ static int on_expose(TickitWindow *win, TickitEventFlags flags, void *_info, voi
 	tickit_renderbuffer_savepen(rb);
 
 	if(!penColors[0].pen_fg)
-      penColors[0].pen_fg = tickit_pen_new_attrs(
-          TICKIT_PEN_FG, penColors[0].val,
-          0);
+      penColors[0].pen_fg = tickit_pen_new_attrs(TICKIT_PEN_FG, penColors[0].val, 0);
 	
+	
+	// tickit_pen_set_colour_attr_rgb8(penColors[0].pen_fg, TICKIT_PEN_FG, (TickitPenRGB8){.r=10,.g=50,.b=200});
+
+	tickit_pen_set_colour_attr_desc(penColors[0].pen_fg, TICKIT_PEN_FG, "hi-cyan");
 	tickit_renderbuffer_setpen(rb, penColors[0].pen_fg);
-	
+
 	// Command to render text to the screen. 
 	// TODO: This doesn't print text yet and I don't know why
 	//		--> I had this function as on_focus instead of on_expose
 	tickit_renderbuffer_textf(rb, "blue foreground? %s", penColors[0].name);
 
+	for (int i = 0; i < 200; ++i)
+	{
+		tickit_renderbuffer_text(rb, ".");
+	}
+
+	// fancy line :)
+	tickit_renderbuffer_hline_at(rb, 3, 0, 30, TICKIT_LINE_DOUBLE, 0);
+
 	tickit_renderbuffer_restore(rb);
 
 	tickit_renderbuffer_text(rb, "     ");
 
+
+	return 1;
+}
+
+static int checksuspend(TickitWindow *win, TickitEventFlags flags, void *_info, void *data)
+{
+	TickitKeyEventInfo *info = _info;
+	if (info->mod != TICKIT_MOD_CTRL || !streq(info->str, "C-z")) return 0;
+
+	TickitTerm *term = tickit_window_get_term(win);
+
+	tickit_term_pause(term);
+	raise(SIGSTOP);
+	tickit_term_resume(term);
+	tickit_window_expose(win, NULL);
 
 	return 1;
 }
@@ -85,6 +115,7 @@ int main(int argc, char *argv[])
   	}
 
 	tickit_window_bind_event(root, TICKIT_WINDOW_ON_EXPOSE, 0, &on_expose, NULL);
+	tickit_window_bind_event(root, TICKIT_WINDOW_ON_KEY, 0, &checksuspend, NULL);
 
 	tickit_run(t);
 
