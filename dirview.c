2fee143dedad744c26931c61302dcbe1044e8ac8
LollosaurusRex <ethanrex1@gmail.com>
2024-04-04 21:09:00 -0500

    Key input shows single char

--- dirview.c
+++ dirview.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <string.h>
+#include <strings.h>
 
 
 #include <tickit.h>
@@ -11,6 +13,11 @@
 #define streq(a,b) (!strcmp(a,b))
 
 TickitPenRGB8 blue = {.r = 10, .g = 100, .b = 200};
+
+/**
+ * If I want to do fuzzy searching on local files, CLI tool fzf
+ * 
+ */
 
 /**
  * I've never seen a struct created like this before.
@@ -40,8 +47,46 @@ struct
 };
 
 
-// All window bind events will have this form
-static int on_expose(TickitWindow *win, TickitEventFlags flags, void *_info, void *data)
+TickitRect termRect;
+
+TickitWindow *root;
+TickitWindow *typer;
+
+// Buffer for the text we store in the typer. Size determined by 2*cols.
+// Declared first in typerOnExpose.
+// free in main.
+// Size is 2*cols for flexibility on window resize (not implemented currently)
+char *typerBuffer; 
+unsigned short numTyped = 0;
+
+TickitKeyEventInfo lastKey;
+
+// Function Declarations
+// Events
+static int rootOnExpose	(TickitWindow *win, TickitEventFlags flags, void *_info, void *data);
+static int rootOnKey	(TickitWindow *win, TickitEventFlags flags, void *_info, void *data);
+static int typerOnExpose(TickitWindow *win, TickitEventFlags flags, void *_info, void *data);
+
+
+// TODO: Typer window at bottom, for user to type text in
+// TODO: Find a way to make the program structure not fucked 
+//			(headers? .o's?) (before it gets too late) (make a graph?)
+// TODO: 
+
+// Low priority or unimportant right now (focus on something else)
+// TODO: Implement window resize considerations (low priority)
+
+
+/**
+ * @brief Expose event function called for root on spawn and any subsequent times expose is called. 
+ * 
+ * @param win 
+ * @param flags 
+ * @param _info 
+ * @param data 
+ * @return int 
+ */
+static int rootOnExpose (TickitWindow *win, TickitEventFlags flags, void *_info, void *data)
 {
 	TickitExposeEventInfo *info = _info;
 	TickitRenderBuffer *rb = info->rb;
@@ -73,10 +118,14 @@ static int on_expose(TickitWindow *win, TickitEventFlags flags, void *_info, voi
 
 
 	TickitPen *circlePen = tickit_pen_new();
+	
 	bool a1 = tickit_pen_set_colour_attr_desc(circlePen, TICKIT_PEN_FG, "white");
 	bool a2 = tickit_pen_set_colour_attr_desc(circlePen, TICKIT_PEN_BG, "hi-cyan");
 
-	
+	TickitPen *cursorPen = tickit_pen_new();
+	tickit_pen_set_colour_attr_desc(cursorPen, TICKIT_PEN_FG, "hi-white");
+	tickit_pen_set_colour_attr_desc(cursorPen, TICKIT_PEN_BG, "grey");
+	tickit_pen_set_bool_attr(cursorPen, TICKIT_PEN_BLINK, true);
 	
 	tickit_pen_set_bool_attr(circlePen, (TickitPenAttr) TICKIT_PEN_STRIKE, false);
 	tickit_renderbuffer_setpen(rb, circlePen);
@@ -123,20 +172,76 @@ static int on_expose(TickitWindow *win, TickitEventFlags flags, void *_info, voi
 	tickit_renderbuffer_text(rb, "     ");
 
 
+	tickit_renderbuffer_setpen(rb, cursorPen);
+	tickit_renderbuffer_text_at(rb, lines-1, 0, "> ");
+
 	return 1;
 }
 
-static int checksuspend(TickitWindow *win, TickitEventFlags flags, void *_info, void *data)
+/**
+ * @brief Event called when the root window experiences a key.
+ * Modifies `lastKey` global variable.
+ * Check if CTRL+Z was pressed, if so pauses the terminal. Can be replaced with `fg`. 
+ * 
+ * @param win root
+ * @param flags 
+ * @param _info TickitKeyEventInfo
+ * @param data 
+ * @return int 
+ */
+static int rootOnKey(TickitWindow *win, TickitEventFlags flags, void *_info, void *data)
 {
 	TickitKeyEventInfo *info = _info;
-	if (info->mod != TICKIT_MOD_CTRL || !streq(info->str, "C-z")) return 0;
 
-	TickitTerm *term = tickit_window_get_term(win);
+	if(lastKey.str) free((void *)lastKey.str);
 
-	tickit_term_pause(term);
-	raise(SIGSTOP);
-	tickit_term_resume(term);
-	tickit_window_expose(win, NULL);
+	lastKey = *info;
+	lastKey.str = strdup(info->str);
+
+	if (info->mod == TICKIT_MOD_CTRL && streq(info->str, "C-z")) 
+	{
+		TickitTerm *term = tickit_window_get_term(win);
+		tickit_term_pause(term);
+		raise(SIGSTOP);
+		tickit_term_resume(term);
+		tickit_window_expose(win, NULL);
+		// return 0;
+	}
+
+	tickit_window_expose(typer, (TickitRect*) NULL);
+
+	return 1;
+}
+
+static int typerOnExpose(TickitWindow *win, TickitEventFlags flags, void *_info, void *data)
+{
+	TickitExposeEventInfo *info = _info;
+
+	TickitRenderBuffer *rb = info->rb;
+	TickitRect rect = info->rect;
+	
+	if (!typerBuffer)
+	{
+		typerBuffer = malloc(rect.cols * sizeof(char));
+	}
+
+	if (lastKey.str == NULL) return 0;
+
+	
+
+	// printf("typed exposed\n");
+
+	tickit_renderbuffer_goto(rb, 0, 0);
+	tickit_renderbuffer_clear(rb);
+
+	TickitPen *p = tickit_pen_new();
+	tickit_pen_set_colour_attr_desc(p, TICKIT_PEN_FG, "white");
+	tickit_renderbuffer_setpen(rb, p);
+
+	tickit_renderbuffer_text(rb, "> ");
+
+	tickit_renderbuffer_text(rb, lastKey.str);
+	// tickit_renderbuffer_textf(rb, "%d", tickit_renderbuffer_get_span(rb, 0, 0, NULL, NULL, 20));
 
 	return 1;
 }
@@ -153,21 +258,33 @@ int main(int argc, char *argv[])
 		return 1;
     }
 
-	TickitWindow *root = tickit_get_rootwin(t);
+	root = tickit_get_rootwin(t);
 	if(!root) 
 	{
 		fprintf(stderr, "Cannot create root window - %s\n", strerror(errno));
 		return 1;
   	}
+	termRect = tickit_window_get_geometry(root);
+	tickit_window_bind_event(root, TICKIT_WINDOW_ON_EXPOSE, (TickitBindFlags) 0, &rootOnExpose, NULL);
+	// tickit_window_bind_event(root, TICKIT_WINDOW_ON_KEY, (TickitBindFlags) 0, &checksuspend, NULL);
+	
 
-	tickit_window_bind_event(root, TICKIT_WINDOW_ON_EXPOSE, (TickitBindFlags) 0, &on_expose, NULL);
-	tickit_window_bind_event(root, TICKIT_WINDOW_ON_KEY, (TickitBindFlags) 0, &checksuspend, NULL);
+	typer = tickit_window_new(root, (TickitRect){.top = termRect.lines-1, .left = 0, .lines = 2, .cols = termRect.cols}, (TickitWindowFlags) 0);
+	if (!typer) 
+	{
+		fprintf(stderr, "Cannot create typer window - %s\n", strerror(errno));
+		return 1;
+	}
+	tickit_window_bind_event(root, (TickitWindowEvent) TICKIT_WINDOW_ON_KEY, (TickitBindFlags) 0, &rootOnKey, NULL);
+	tickit_window_bind_event(typer, TICKIT_WINDOW_ON_EXPOSE, (TickitBindFlags) 0, &typerOnExpose, NULL);
 	
 	tickit_run(t);
 
 	tickit_window_close(root);
 
 	tickit_unref(t);
+
+	free(typerBuffer);
 
     return EXIT_SUCCESS;
 }
